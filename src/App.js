d29af3dbdc3441742f4c82b8c38d87b6873ab0cd
fredrikpihlgren <70364712+fredrikpihlgren@users.noreply.github.com>
2021-05-24 20:44:25 +0200

    Added navbar with working links and css-class for highlight

--- src/App.js
+++ src/App.js
@@ -1,24 +1,60 @@
-import logo from './logo.svg';
+import React from 'react';
+import {useState} from 'react';
 import './App.css';
+import {BrowserRouter as Router, Link, Switch, Route} from 'react-router-dom';
 
 function App() {
+
+  const [navActive, setNavActive] = useState(-1);
+
+  const menuOptions = [
+    {url: '/', name: 'Startsida'},
+    {url: '/battle', name: 'Tävla'},
+    {url: '/gallery', name: 'Galleri'},
+    {url: '/statistics', name: 'Statistik'},
+    {url: '/history', name: 'Historik'}
+  ]
+
+  const navOptions = menuOptions.map((param, index) => {
+    return (
+      <Link key={param.name} to={param.url} onClick={() => setme(index)} className={(navActive === index) ? 'active' : ''}>{param.name}</Link>
+    )
+  });
+
+  function setme(param) {
+    setNavActive(param);
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <nav>
+            {navOptions}
+          </nav>
+        </header>
+        <main>
+
+        före switch
+        
+        <Switch>
+          {/* add props into component via route:
+          render={() => <Example prop1={data}/>}
+          */}
+          <Route path="/history">HISTORIK</Route>
+          <Route path="/statistics">STATS MAN!</Route>
+          <Route path="/gallery">GALLERI</Route>
+          <Route path="/battle">BATTLE TIME!</Route>
+          <Route path="/">Välkommen ska du vara, din gris</Route>
+        </Switch>
+        
+        
+        efter switch
+
+
+        </main>
+      </div>
+    </Router>
   );
 }
 
